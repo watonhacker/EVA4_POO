bbdda5bdcce00d88377094667004534d71a1035e
Cristóbal Acevedo <cristobalacevedom@gmail.com>
2022-01-02 20:47:25 -0300

    Menú

---
+++ main.py
@@ -0,0 +1,7 @@
+
+print("Menú Principal")
+print("1.Creación de encomiendas")
+print("2.Consulta de encomiendas")
+print("3.Actualización de estado de encomiendas")
+print("4.Eliminación de encomiendas")
+print("5.Salir")
