81a02899c01c285daf3aa8c39228855024e6529c
Cristóbal Acevedo <cristobalacevedom@gmail.com>
2022-01-03 17:28:04 -0300

    Ultimo

--- vistas/Vistas.py
+++ vistas/Vistas.py
@@ -1,56 +1,55 @@
 from EVA4_POO.DAO.database import DAO
 
-def validarEntero(pregunta):
-    while True:
-        try:
-            numero = int(input(pregunta))
-        except ValueError:
-            print("Debes escribir un número.")
-            continue
-
-        if type(numero) == float:
-            print("Debes escribir un numero entero")
-            continue
-
-        if numero < 0:
-            print("Debes escribir un número positivo.")
-            continue
-
-        else:
-            break
-    return numero
-
-def validarFloat(pregunta):
-    while True:
-        try:
-            numero = float(input(pregunta))
-        except ValueError:
-            print("Debes escribir un número.")
-            continue
-
-        if numero < 0:
-            print("Debes escribir un número positivo.")
-            continue
-
-        else:
-            break
-    return numero
-# Funcioones que interactuen con el usuario
-
 class Vista():
 
     def __init__(self):
         self.DAOAccess = DAO()
 
+    def validarEntero(self, pregunta):
+        while True:
+            try:
+                numero = int(input(pregunta))
+            except ValueError:
+                print("Debes escribir un número.")
+                continue
+
+            if type(numero) == float:
+                print("Debes escribir un numero entero")
+                continue
+
+            if numero < 0:
+                print("Debes escribir un número positivo.")
+                continue
+
+            else:
+                break
+        return numero
+
+    def validarFloat(self, pregunta):
+        while True:
+            try:
+                numero = float(input(pregunta))
+            except ValueError:
+                print("Debes escribir un número.")
+                continue
+
+            if numero < 0:
+                print("Debes escribir un número positivo.")
+                continue
+
+            else:
+                break
+        return numero
+
     def datosCrearEncomienda(self):
         print("Formulario de creación de Encomienda:")
         remitente = input("Ingrese el remitente: ")
         destinatario = input("Ingrese el destinatario: ")
-        peso = validarFloat("Ingrese el peso: ")
-        alto = validarFloat("Ingrese el alto: ")
-        largo = validarFloat("Ingrese el largo: ")
-        ancho = validarFloat("Ingrese el ancho: ")
-        valordeclarado = validarEntero("Ingrese el valor declarado: ")
+        peso = self.validarFloat("Ingrese el peso: ")
+        alto = self.validarFloat("Ingrese el alto: ")
+        largo = self.validarFloat("Ingrese el largo: ")
+        ancho = self.validarFloat("Ingrese el ancho: ")
+        valordeclarado = self.validarEntero("Ingrese el valor declarado: ")
 
         self.DAOAccess.crearEncomienda(remitente, destinatario, peso, alto, largo, ancho, valordeclarado)
         print("La encomienda ha sido creada con éxito")
@@ -60,21 +59,21 @@ class Vista():
         self.DAOAccess.eliminarEncomienda(idEliminar)
 
     def datosActualizarEncomienda(self):
-        idActualizar = validarEntero("Ingrese el id de la encomienda a actualizar: ")
+        idActualizar = self.validarEntero("Ingrese el id de la encomienda a actualizar: ")
         existeEncomienda = self.DAOAccess.leerEncomienda(idActualizar)
 
         while existeEncomienda == None:
             print("La encomienda N°{} no ha sido encontrada".format(idActualizar))
-            idActualizar = validarEntero("Ingrese el id de la encomienda a actualizar: ")
+            idActualizar = self.validarEntero("Ingrese el id de la encomienda a actualizar: ")
             existeEncomienda = self.DAOAccess.leerEncomienda(idActualizar)
 
         remitente = input("Ingrese el remitente: ")
         destinatario = input("Ingrese el destinatario: ")
-        peso = validarFloat("Ingrese el peso: ")
-        alto = validarFloat("Ingrese el alto: ")
-        largo = validarFloat("Ingrese el largo: ")
-        ancho = validarFloat("Ingrese el ancho: ")
-        valorDeclarado = validarEntero("Ingrese el valor declarado: ")
+        peso = self.validarFloat("Ingrese el peso: ")
+        alto = self.validarFloat("Ingrese el alto: ")
+        largo = self.validarFloat("Ingrese el largo: ")
+        ancho = self.validarFloat("Ingrese el ancho: ")
+        valorDeclarado = self.validarEntero("Ingrese el valor declarado: ")
         self.DAOAccess.actualizarEncomienda(idActualizar, remitente, destinatario, peso, alto, largo, ancho, valorDeclarado)
         print("La encomienda {} ha sido actualizada".format(idActualizar))
 
@@ -84,7 +83,7 @@ class Vista():
         return self.DAOAccess.leerEncomiendas()
 
     def traerEncomienda(self):
-        idEncomienda = validarEntero("Ingrese el id de la encomienda: ")
+        idEncomienda = self.validarEntero("Ingrese el id de la encomienda: ")
         encomiendaEncontrada = self.DAOAccess.leerEncomienda(idEncomienda)
         if encomiendaEncontrada == None:
             print("No se ha encontrado la encomienda N°{}".format(idEncomienda))
